9ed590d7633493cf3dca9f22365b64456e16e7db
Nevo Band <nevoband@gmail.com>
2019-02-13 16:06:51 -0600

    added molecule for testing

---
+++ molecule/default/tests/test_common.py
@@ -0,0 +1,18 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('file, content', [
+  ("/etc/automysqlbackup/automysqlbackup.conf", "Ansible managed"),
+  ("/usr/local/bin/automysqlbackup", "AutoMySQLBackup version"),
+])
+def test_files(host, file, content):
+    file = host.file(file)
+
+    assert file.exists
+    assert file.contains(content)
