a174bef58aec9ee431f4fc817bcb87452033196f
Nevo Band <nevoband@gmail.com>
2019-02-13 16:55:56 -0600

    added test for cron

--- molecule/default/tests/test_common.py
+++ molecule/default/tests/test_common.py
@@ -10,6 +10,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 @pytest.mark.parametrize('file, content', [
   ("/etc/automysqlbackup/automysqlbackup.conf", "Ansible managed"),
   ("/usr/local/bin/automysqlbackup", "AutoMySQLBackup version"),
+  ("/etc/cron.d/ansible_automysqlbackup",
+      "root /usr/local/bin/automysqlbackup")
 ])
 def test_files(host, file, content):
     file = host.file(file)
